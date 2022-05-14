c8585f57ccabe9bd7e9f595b4282bd52dd33aba4
vniroshan <nnirosh447@gmail.com>
2022-05-14 16:17:45 +0530

    test 23

--- src/components/Todo/NoTasks.vue
+++ src/components/Todo/NoTasks.vue
@@ -3,7 +3,7 @@
    
      class="no-task">
       <v-icon size="100" color="primary"> mdi-check </v-icon>
-      <div class="text-h5 primary--text">No Tasks 20</div>
+      <div class="text-h5 primary--text">No Tasks 21</div>
     </div>
 </template>
 
